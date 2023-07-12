b1bb4fbd42c3d60b4ae13b5ac3d90286208eb2f1
omair <omairdal@gmail.com>
2023-07-12 18:38:26 +0300

    added icons

--- SalesTasks/components/TasksList.tsx
+++ SalesTasks/components/TasksList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { DirectionalHint, Icon, Stack, Text, TooltipHost, VerticalDivider } from '@fluentui/react';
+import { DirectionalHint, Icon, Stack, Text, TooltipHost } from '@fluentui/react';
 import { task_task_statecode } from '../cds-generated/enums/task_task_statecode';
 import { task_task_prioritycode } from '../cds-generated/enums/task_task_prioritycode';
 
@@ -30,10 +30,16 @@ const MyList: React.FC = () => {
     },
     {
       id: 2, title: 'the second', tasks: [
-        { id: '1', title: 'the first', status: task_task_statecode.Completed, priority: task_task_prioritycode.Low },
+        { id: '1', title: 'the first', status: task_task_statecode.Canceled, priority: task_task_prioritycode.Low },
         { id: '2', title: 'the second', status: task_task_statecode.Completed, priority: task_task_prioritycode.High },
         { id: '3', title: 'the third', status: task_task_statecode.Open, priority: task_task_prioritycode.High },
-        { id: '4', title: 'the fourth', status: task_task_statecode.Open, priority: task_task_prioritycode.Normal }
+        { id: '4', title: 'the fourth', status: task_task_statecode.Canceled, priority: task_task_prioritycode.Normal }
+      ]
+    },
+    {
+      id: 4, title: 'this is a new one to test all completed', tasks: [
+        { id: '1', title: 'this is completed', status: task_task_statecode.Completed, priority: task_task_prioritycode.Normal },
+        { id: '2', title: 'this as well', status: task_task_statecode.Completed, priority: task_task_prioritycode.Low }
       ]
     },
     {
@@ -72,6 +78,10 @@ interface ListItemProps {
 
 //this is a list item for the horizontal list
 const ListItem = ({ item, expanded, onClick }: ListItemProps) => {
+  const itemTitle = React.useMemo(
+    () => `${item.title} (${item.tasks.filter(t => t.status === task_task_statecode.Completed).length}, ${item.tasks.filter(t => t.status === task_task_statecode.Open).length}, ${item.tasks.filter(t => t.status === task_task_statecode.Canceled).length})`,
+    [item])
+
   return (
     <Stack
       onClick={() => onClick(item)}
@@ -79,6 +89,7 @@ const ListItem = ({ item, expanded, onClick }: ListItemProps) => {
       tokens={{ childrenGap: "0.5rem" }}
       styles={{
         root: {
+          cursor: 'pointer',
           width: expanded ? "300px" : "3ch",
           height: "100%",
           background: "rgba(255,73, 51, 0.1)",
@@ -107,8 +118,11 @@ const ListItem = ({ item, expanded, onClick }: ListItemProps) => {
               }
             }}
           >
-            <TooltipHost content={item.title} directionalHint={DirectionalHint.bottomCenter}>
-              {item.title}
+            <TooltipHost
+              content={itemTitle}
+              directionalHint={DirectionalHint.bottomCenter}
+            >
+              {itemTitle}
             </TooltipHost>
           </Text>
         )}
@@ -123,31 +137,53 @@ const ListItem = ({ item, expanded, onClick }: ListItemProps) => {
         />
       </Stack>
       {expanded ? item.tasks.map((task, index) => (
-        <Text
-          variant="medium"
-          styles={{
-            root: {
-              fontWeight: 600,
-              textOverflow: "ellipsis",
-              overflow: "hidden",
-              whiteSpace: 'nowrap'
-            }
-          }}
-        >
-          <TooltipHost content={task.title} directionalHint={DirectionalHint.bottomCenter}>
-            {task.title}
-          </TooltipHost>
-        </Text>
-      ))
-        : (
-          <Stack horizontalAlign='center' styles={{ root: { width: "100%", height: "100%" } }}>
+        <Stack horizontal horizontalAlign='space-around' styles={{ root: { width: '100%' } }}>
+          <Stack grow horizontalAlign='start'>
             <Text
               variant="medium"
               styles={{
                 root: {
                   fontWeight: 600,
-                  transform: "translateY(calc(215px)) translateX(-0.1em) rotate(-90deg)",
-                  width: '250px',
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: 'nowrap'
+                }
+              }}
+            >
+              <TooltipHost
+                content={task.title}
+                directionalHint={DirectionalHint.bottomCenter}
+              >
+                {task.title}
+              </TooltipHost>
+            </Text>
+          </Stack>
+          <Stack>
+            <Icon
+              styles={{ root: { color: task.status === task_task_statecode.Open ? "orange" : task.status === task_task_statecode.Canceled ? "red" : "#009900", fontWeight: 900 } }}
+              iconName={
+                task.status === task_task_statecode.Open ? "Warning" : task.status === task_task_statecode.Canceled ? "ErrorBadge" : "CheckMark"
+              }
+            />
+          </Stack>
+        </Stack>
+      ))
+        : (
+          <Stack horizontalAlign='center' styles={{ root: { width: "100%", height: "100%" } }}>
+            {item.tasks.some(task => task.status === task_task_statecode.Canceled) ? (
+              <Icon styles={{ root: { color: "red" } }} iconName="ErrorBadge" />
+            ) : item.tasks.some(task => task.status === task_task_statecode.Open) ? (
+              <Icon styles={{ root: { color: "orange" } }} iconName="Warning" />
+            ) : (
+              <Icon styles={{ root: { color: "#009900" } }} iconName="CheckMark" />
+            )}
+            <Text
+              variant="medium"
+              styles={{
+                root: {
+                  fontWeight: 600,
+                  transform: "translate(-0.1em, 165px) rotate(-90deg)",
+                  width: '320px',
                   writingMode: "vartical-lr",
                   textOverflow: "ellipsis",
                   overflow: "hidden",
@@ -155,8 +191,11 @@ const ListItem = ({ item, expanded, onClick }: ListItemProps) => {
                 }
               }}
             >
-              <TooltipHost content={item.title} directionalHint={DirectionalHint.bottomCenter}>
-                {item.title}
+              <TooltipHost
+                content={itemTitle}
+                directionalHint={DirectionalHint.bottomCenter}
+              >
+                {itemTitle}
               </TooltipHost>
             </Text>
           </Stack>
